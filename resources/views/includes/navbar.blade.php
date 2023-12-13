7c1fcc1b6f1e7f48a10ffc5b8855238495f5de02
Herlin Priatna <herlinpriatna@student.telkomuniversity.ac.id>
2023-12-13 19:13:08 +0700

    menambahkan profil

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -58,6 +58,7 @@
                     <a href="#" class="text-white text-decoration-none px-4 py-2">
                         {{ Auth::user()->username }}
                     </a>
+                    
                 @endguest
                     </div>
                 </div>
